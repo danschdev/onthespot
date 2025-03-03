6e356b7be75ca964390b437ad0f50975ab40836f
Daniel Schnelle <daniel_schnelle@web.de>
2025-03-03 18:33:53 +0100

    Get access token using GuzzleHttp client

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-    echo "Welcome to the jungle";
+require 'vendor/autoload.php';
+require 'logindata.php';
+use GuzzleHttp\Client;
+
+$client = new Client();
+$response = $client->post('https://accounts.spotify.com/api/token', [
+    'form_params' => [
+        'grant_type' => 'client_credentials'
+    ],
+    'headers' => [
+        'Authorization' => 'Basic ' . base64_encode($clientId.":".$clientSecret),
+        'Content-Type' => 'application/x-www-form-urlencoded'
+    ]
+]);
+
+$body = json_decode($response->getBody(), true);
+$token = $body['access_token'];
+
+var_dump($token);
 ?>
