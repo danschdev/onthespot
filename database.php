053eb24c0fd3d5953049def59f24fcaf8788f734
Daniel Schnelle <daniel_schnelle@web.de>
2025-03-14 16:30:23 +0100

    Run CS Fixer

--- database.php
+++ database.php
@@ -1,11 +1,12 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=onthespot;charset=utf8mb4',
-    'root', '');
+    $db = new PDO(
+        'mysql:host=localhost;dbname=onthespot;charset=utf8mb4',
+        'root',
+        ''
+    );
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-
-catch(PDOException $exception) {
+} catch (PDOException $exception) {
     echo 'Konnte Verbindung nicht erstellen: '.$exception->getMessage();
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'vendor/autoload.php';
 require 'database.php';
 use GuzzleHttp\Client;
@@ -16,8 +17,9 @@ $artists = [];
 $trackitems = [];
 do {
 
-    $response = $client->request('GET',
-    'https://api.spotify.com/v1/playlists/'
+    $response = $client->request(
+        'GET',
+        'https://api.spotify.com/v1/playlists/'
         .$_ENV['PLAYLIST_ID']
         ."/tracks?offset=$offset&limit=100",
         [
@@ -31,9 +33,9 @@ do {
 
     $offset += 100;
 
-    foreach($songs->items as $item) {
+    foreach ($songs->items as $item) {
         $trackitems[] = $item;
-        foreach($item->track->artists as $artist) {
+        foreach ($item->track->artists as $artist) {
             if (key_exists($artist->id, $artists) && key_exists("count", $artists[$artist->id])) {
                 $artists[$artist->id]["count"] += 1;
                 $artists[$artist->id]["name"] = $artist->name;
@@ -44,13 +46,13 @@ do {
             }
         }
     }
-} while ( 0 == sizeof($songs->items) || 99 <= sizeof($songs->items)) ; // Delete first condition? Loop will be run >= 1 time anyway. Test with exactly 100 tracks!
+} while (0 == sizeof($songs->items) || 99 <= sizeof($songs->items)) ; // Delete first condition? Loop will be run >= 1 time anyway. Test with exactly 100 tracks!
 
-uasort($artists, function($a, $b) {
+uasort($artists, function ($a, $b) {
     return $a["count"] < $b["count"] ? 1 : -1;
 });
 
-foreach($artists as $key => $artist) {
+foreach ($artists as $key => $artist) {
     echo $artist["name"].": ".$artist["count"];
     echo "<br/>";
     $sql = "INSERT INTO artists
@@ -61,35 +63,38 @@ foreach($artists as $key => $artist) {
 }
 
 echo "<table style='borderwidth: 2px borderstyle: solid'>\n";
-foreach($trackitems as $item) {
+foreach ($trackitems as $item) {
     echo "<tr>";
-        echo "<td>";
-            if ($item->track->explicit) { echo "&#x26A0; ";};
-            echo($item->track->name);
-        echo "</td>";
-        echo "<td>";
-            echo($item->track->artists[0]->name);
-        echo "</td>";
-        echo "<td><img src='";
-            echo($item->track->album->images[2]->url);
-        echo "'></td>";
-        echo "<td>";
-            echo($item->track->album->name);
-        echo "</td>";
-        foreach (array_slice($item->track->artists, 1) as $artist) {
-            echo("<td>$artist->name</td>");
-        }
+    echo "<td>";
+    if ($item->track->explicit) {
+        echo "&#x26A0; ";
+    };
+    echo($item->track->name);
+    echo "</td>";
+    echo "<td>";
+    echo($item->track->artists[0]->name);
+    echo "</td>";
+    echo "<td><img src='";
+    echo($item->track->album->images[2]->url);
+    echo "'></td>";
+    echo "<td>";
+    echo($item->track->album->name);
+    echo "</td>";
+    foreach (array_slice($item->track->artists, 1) as $artist) {
+        echo("<td>$artist->name</td>");
+    }
     echo "</tr>\n";
 }
 echo "</table>";
 
-function accesstoken(Client $client): string {
+function accesstoken(Client $client): string
+{
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
-    
+
     $clientId = $_ENV['SPOTIFY_CLIENT_ID'];
     $clientSecret = $_ENV['SPOTIFY_CLIENT_SECRET'];
-    
+
     $response = $client->post('https://accounts.spotify.com/api/token', [
         'form_params' => [
             'grant_type' => 'client_credentials'
@@ -99,10 +104,7 @@ function accesstoken(Client $client): string {
             'Content-Type' => 'application/x-www-form-urlencoded'
         ]
     ]);
-    
+
     $body = json_decode($response->getBody(), true);
     return $body['access_token'];
 }
-
-
-?>
