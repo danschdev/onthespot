75b35affc8261491dd37c457ec6c739191d4dab2
Daniel Schnelle <daniel_schnelle@web.de>
2025-03-14 11:33:04 +0100

    PDO Error Mode setzen

--- database.php
+++ database.php
@@ -3,6 +3,7 @@
 try {
     $db = new PDO('mysql:host=localhost;dbname=onthespot;charset=utf8mb4',
     'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 
 catch(PDOException $exception) {
