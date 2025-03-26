fb189389fa917e836241eaf22236aed7a9499826
Daniel Schnelle <daniel_schnelle@web.de>
2025-03-26 16:39:25 +0100

    Move API token service to separate file

--- src/index.php
+++ src/index.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 require '../vendor/autoload.php';
 
 require 'database.php';
+require 'spotifyApi.php';
+
 use GuzzleHttp\Client;
 
 $client = new Client();
@@ -93,26 +95,3 @@ foreach ($trackitems as $item) {
     echo "</tr>\n";
 }
 echo '</table>';
-
-function accesstoken(Client $client): string
-{
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'\..');
-    $dotenv->load();
-
-    $clientId = $_ENV['SPOTIFY_CLIENT_ID'];
-    $clientSecret = $_ENV['SPOTIFY_CLIENT_SECRET'];
-
-    $response = $client->post('https://accounts.spotify.com/api/token', [
-        'form_params' => [
-            'grant_type' => 'client_credentials',
-        ],
-        'headers' => [
-            'Authorization' => 'Basic '.base64_encode($clientId.':'.$clientSecret),
-            'Content-Type' => 'application/x-www-form-urlencoded',
-        ],
-    ]);
-
-    $body = json_decode($response->getBody()->__toString(), true);
-
-    return $body['access_token'];
-}
