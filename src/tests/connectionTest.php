b00fca706d323b84c06090835085e99413721c2c
Daniel Schnelle <daniel_schnelle@web.de>
2025-03-28 10:07:09 +0100

    Rewrite tests to work with class design

--- src/tests/connectionTest.php
+++ src/tests/connectionTest.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 require_once __DIR__.'/../database.php';
@@ -13,15 +12,16 @@ final class connectionTest extends TestCase
 {
     public function testDatabaseConnection(): void
     {
-        $pdo = getDatabaseConnection();
+        $database = new Database();
+        $pdo = $database->getPdo();
         self::assertInstanceOf(PDO::class, $pdo);
         self::assertTrue(false !== $pdo->getAttribute(PDO::ATTR_CONNECTION_STATUS));
     }
 
     public function testSpotifyApiToken(): void
     {
-        $client = new Client();
-        $token = accesstoken($client);
+        $spotifyApi = new SpotifyApi();
+        $token = $spotifyApi->createAccessToken();
         self::assertNotEmpty($token);
     }
 }
