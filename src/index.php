48fc1b8253928b6e9a6f327e7404d169463fa787
Daniel Schnelle <daniel_schnelle@web.de>
2025-05-10 15:08:50 +0200

    Remove single track data

--- src/index.php
+++ src/index.php
@@ -68,28 +68,3 @@ foreach ($artists as $key => $artist) {
     echo '<br/>';
     $spotifyRepository->saveArtist($key, $artist);
 }
-
-echo "<table style='borderwidth: 2px borderstyle: solid'>\n";
-foreach ($trackitems as $item) {
-    echo '<tr>';
-    echo '<td>';
-    if ($item->track->explicit) {
-        echo '&#x26A0; ';
-    }
-    echo $item->track->name;
-    echo '</td>';
-    echo '<td>';
-    echo $item->track->artists[0]->name;
-    echo '</td>';
-    echo "<td><img src='";
-    echo $item->track->album->images[2]->url;
-    echo "'></td>";
-    echo '<td>';
-    echo $item->track->album->name;
-    echo '</td>';
-    foreach (array_slice($item->track->artists, 1) as $artist) {
-        echo "<td>{$artist->name}</td>";
-    }
-    echo "</tr>\n";
-}
-echo '</table>';
